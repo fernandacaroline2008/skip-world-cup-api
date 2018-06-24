5a9174ce906b54be2a950adc8a042152eb70fdd8
Fernanda Caroline <fcsantos@fitec.org.br>
2018-06-24 10:21:22 -0300

    Spring boot configuration

---
+++ src/main/java/com/skipthedishes/skipworldcupapi/SkipWorldCupApiApplication.java
@@ -0,0 +1,12 @@
+package com.skipthedishes.skipworldcupapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkipWorldCupApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkipWorldCupApiApplication.class, args);
+	}
+}
